e59429326604fa75baf6f21debc1f5e922a674e3
Matthew <matthewvandyke08@gmail.com>
2021-03-20 12:47:52 -0700

    Added BrainDUMP

--- pages/index.js
+++ pages/index.js
@@ -48,9 +48,19 @@ export default function Home() {
           <p style={{ paddingLeft: '15px' }} className={styles.text}>Computer science and coding courses made for middle school and high school students, created by students.</p>
           <div style={{ display: "inline-flex", alignItems: "center" }}>
             <div className={styles.tag}>
-              <p style={{ fontSize: '15px', textAlign: 'center', paddingTop: '5px' }} className={styles.text}>Working On</p>
+              <p style={{ fontSize: '15px', textAlign: 'center', paddingTop: '5px' }} className={styles.text}>Visit Now</p>
             </div>
             <Link href="/swcnotes"><img style={{ display: "inline-block", cursor: 'pointer' }} src="/notes.svg"></img></Link>
+          </div>
+        </div>
+
+        <br></br>
+
+        <div className={styles.card}>
+          <p className={styles.smallheader}>BrainDUMP<Link href="">&rarr;</Link></p>
+          <p style={{ paddingLeft: '15px' }} className={styles.text}>BrainDUMP is my most recent project, where users can log in to keep track and store their most recent thoughts. After wanting one place to store my random thoughts and newest ideas I created BrainDUMP to create a tool tailored to just what I need. </p>
+          <div className={styles.tag}>
+            <p style={{ fontSize: '15px', textAlign: 'center', paddingTop: '5px' }} className={styles.text}>Working On</p>
           </div>
         </div>
 
@@ -75,6 +85,7 @@ export default function Home() {
         </div>
 
         <br></br>
+
 
         <footer>
           <Link href="https://github.com/matt1208"><img style={{ cursor: 'pointer' }} src="github.png"></img></Link>
